c971da647d488e498378af5c8e98f281f860053e
unknown <9312cat@gmail.com>
2019-04-13 18:33:25 +0900

    두번쨰로 큰 정수

--- proj_20190407_2.java
+++ proj_20190407_2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class proj_20190407_2 {
 
+	//정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
---
+++ proj_20190413_2.java
@@ -0,0 +1,29 @@
+package gitPro;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class proj_20190413_2 {
+
+	//세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오. 
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("정수 입력: ");
+		int a = scan.nextInt();
+		
+		System.out.print("정수 입력: ");
+		int b = scan.nextInt();
+		
+		System.out.print("정수 입력: ");
+		int c = scan.nextInt();
+		
+		int arrN[] = {a,b,c};
+		
+		Arrays.sort(arrN);
+		
+		System.out.println(arrN[1]);
+	}
+
+}
