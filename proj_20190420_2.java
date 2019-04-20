5eb0922e1d61ec56e931e4230e18cf786bfb7832
unknown <9312cat@gmail.com>
2019-04-20 16:02:30 +0900

    아스키코드

---
+++ proj_20190420_2.java
@@ -0,0 +1,16 @@
+package gitPro;
+
+import java.io.IOException;
+
+public class proj_20190420_2 {
+
+	//주어진 글자의 아스키 코드 출력
+	public static void main(String[] args) throws IOException{
+
+		System.out.print("입력 : ");	
+		int num = System.in.read();	
+		System.out.println("출력 : " + num);
+		
+	}
+
+}
